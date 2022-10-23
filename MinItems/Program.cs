0a41ccedab5bacf1d3cbe5f59436da5862b31a25
Waseem Khalid <waseemsabra2@gmail.com>
2022-10-23 23:17:24 +0300

    Add project files.

---
+++ MinItems/Program.cs
@@ -0,0 +1,36 @@
+﻿
+List<int> items = new List<int> { 6, 1, 3, 0, 2, 4, 5 };
+
+MinItems.getMin(items);
+
+public class MinItems
+{
+
+    public static void getMin(List<int> list)
+    {
+
+        int temp = 0;
+        for (int i = 0; i < list.Count; i++)
+        {
+            for (int j = i+1; j < list.Count; j++)
+            {
+                if(list[j] < list[i])
+                {
+                    temp = list[i];
+                    list[i] = list[j];
+                    list[j] = temp;
+
+                }
+            }
+
+            Console.WriteLine(list[i]);
+        }
+
+
+    }
+}
+
+
+
+
+
